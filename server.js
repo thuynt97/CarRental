f6a7a317448673be06b93bded3de02e9bbb9b953
ThuyNT <thuynguyenbk60@gmail.com>
2020-01-15 16:00:29 +0700

    update server

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require('path');
 const port = 3000
 // Serve static files....
-app.use(express.static(__dirname + '/dist/DataTable'));
+app.use(express.static(__dirname + '/dist/carrental'));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/DataTable/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/carrental/index.html'));
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
