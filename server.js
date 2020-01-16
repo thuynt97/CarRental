481e040e2c3e840541176e49d54bcfcba4d79871
ThuyNT <thuynguyenbk60@gmail.com>
2020-01-16 09:47:16 +0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express')
+//Install express server
+const express = require("express");
+const path = require("path");
+
 const app = express();
-const path = require('path');
-const port = 3000
-// Serve static files....
-app.use(express.static(__dirname + '/dist/carrental'));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/carrental/index.html'));
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/<name-of-app>"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
