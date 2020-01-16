9007622e0af8c819c5925722435bd1084bfb177d
ThuyNT <thuynguyenbk60@gmail.com>
2020-01-16 10:51:25 +0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-//Install express server
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const api = require('./server/routes/api');
+
+const port = 3000;
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+app.use('/api', api);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, function() {
+    console.log('server running on localhost:' + port);
+});
