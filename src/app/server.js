16e1fd5ee8129184076453dc70962f012d4c0106
ThuyNT <thuynguyenbk60@gmail.com>
2020-01-16 16:45:03 +0700

    deploy

--- src/app/server.js
+++ src/app/server.js
@@ -1,15 +1,14 @@
-//Install express server
-const express = require("express");
-const path = require("path");
-
+const path = require('path');
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+// Serve static files
+app.use(express.static(__dirname + '/dist/carrental'));
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/carrental/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
